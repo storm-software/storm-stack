b557ed8062911905eb9cd74eb251413d4ae58d4b
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2023-12-25 19:51:00 -0500

    fix(logging): Temporarily remove the config for logging

--- packages/logging/src/utilities/get-transports.ts
+++ packages/logging/src/utilities/get-transports.ts
@@ -1,14 +1,5 @@
 import { StormConfig } from "@storm-software/config-tools";
-import {
-  StormDateTime,
-  formatDate,
-  formatDateTime
-} from "@storm-stack/date-time";
-import {
-  EMPTY_STRING,
-  isRuntimeServer,
-  isSetString
-} from "@storm-stack/utilities";
+import { isSetString } from "@storm-stack/utilities";
 import { tmpdir } from "os";
 import { join } from "path";
 import pino, {
@@ -111,7 +102,7 @@ export const getTransports = (
     };
   }
 
-  if (isRuntimeServer()) {
+  /* if (isRuntimeServer()) {
     const pinoServerOptions = {
       options: {
         ...baseOptions,
@@ -137,7 +128,7 @@ Message: {msg}
       }
     };
 
-    targets.push(
+   targets.push(
       ...[
         {
           target: "pino/file",
@@ -237,5 +228,7 @@ Message: {msg}
       targets
     }),
     pretty(prettyOptions)
-  );
+  );*/
+
+  return pino(pretty());
 };
