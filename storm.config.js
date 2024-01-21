e256acb4069633b6e3d3f8239fa24edc5c992e1a
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-01-21 03:49:14 +0000

    fix(unique-identifier): Fixes for the plugin system errors

--- storm.config.js
+++ storm.config.js
@@ -1,30 +1,31 @@
 module.exports = {
-  name: "storm-stack",
-  namespace: "storm-stack",
-  organization: "storm-software",
-  preMajor: false,
-  owner: "@storm-software/development",
-  worker: "stormie-bot",
-  workspaceRoot: "C:\\Development\\storm-ops",
-  runtimeDirectory: "node_modules/.storm",
-  timezone: "America/New_York",
-  locale: "en-US",
-  logLevel: "info",
-  colors: {
-    primary: "#1fb2a6",
-    background: "#1d232a",
-    success: "#087f5b",
-    info: "#0ea5e9",
-    warning: "#fcc419",
-    error: "#990000",
-    fatal: "#7d1a1a"
-  },
-  extensions: {
-    logging: {
-      fileName: "storm",
-      fileExtension: "log",
-      path: "tmp/storm/logs",
-      stacktrace: true
-    }
-  }
+	name: "storm-stack",
+	namespace: "storm-stack",
+	organization: "storm-software",
+	preMajor: false,
+	ci: false,
+	owner: "@storm-software/development",
+	worker: "stormie-bot",
+	workspaceRoot: "C:\\Development\\storm-ops",
+	runtimeDirectory: "node_modules/.storm",
+	timezone: "America/New_York",
+	locale: "en-US",
+	logLevel: "info",
+	colors: {
+		primary: "#1fb2a6",
+		background: "#1d232a",
+		success: "#087f5b",
+		info: "#0ea5e9",
+		warning: "#fcc419",
+		error: "#990000",
+		fatal: "#7d1a1a",
+	},
+	extensions: {
+		logging: {
+			fileName: "storm",
+			fileExtension: "log",
+			path: "tmp/storm/logs",
+			stacktrace: true,
+		},
+	},
 };
