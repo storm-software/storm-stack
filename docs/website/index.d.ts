db95426a208d86013af7a0ec5b695ee5eb65d0d1
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-01-29 08:33:46 +0000

    fix(utilities): Reformat the README markdown files

--- docs/website/index.d.ts
+++ docs/website/index.d.ts
@@ -2,5 +2,6 @@
 declare module "*.svg" {
   const content: any;
   export const ReactComponent: any;
+  // biome-ignore lint/correctness/noUndeclaredVariables: <explanation>
   export default content;
 }
