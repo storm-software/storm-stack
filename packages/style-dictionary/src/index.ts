2c19f269d745562c2082c604454ff6b3765c6cda
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-02-17 10:32:38 +0000

    feat(style-dictionary): Added package with extensions for the `style-dictionary` utility

---
+++ packages/style-dictionary/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * The style-dictionary library used by Storm Software for building TypeScript applications.
+ *
+ * @remarks
+ * A package including various extensions to the Style Dictionary design token generation utility
+ *
+ * @packageDocumentation
+ */
+
+export * from "./config";
