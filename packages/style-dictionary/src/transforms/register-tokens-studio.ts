472e94d9ea22feef08a716031f462f1f01936f34
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-07-26 15:55:15 +0100

    revert(monorepo): Remove deprecated style-dictionary package

--- packages/style-dictionary/src/transforms/register-tokens-studio.ts
+++
@@ -1,6 +0,0 @@
-import { registerTransforms } from "@tokens-studio/sd-transforms";
-import type StyleDictionary from "style-dictionary";
-
-export const registerTokensStudioTransforms = (sd: typeof StyleDictionary) => {
-  registerTransforms(sd as any);
-};
