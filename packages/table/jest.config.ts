f410d346046ebec237873894b68066984ea172a0
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-07-26 15:52:29 +0100

    revert(monorepo): Remove deprecated table package

--- packages/table/jest.config.ts
+++
@@ -1,3 +0,0 @@
-import { getJestConfig } from "@storm-software/testing-tools";
-
-export default getJestConfig("frontend/table", true, "table");
--- packages/table/src/create-table.ts
+++
@@ -1,6 +0,0 @@
-import { createTable as createReactTable } from "@tanstack/react-table";
-import type { Table, TableOptionsResolved } from "@tanstack/table-core";
-
-export const createTable = <T>(config: TableOptionsResolved<T>): Table<T> => {
-  return createReactTable<T>(config);
-};
