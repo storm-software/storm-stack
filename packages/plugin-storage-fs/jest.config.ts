7b15eee38b208cdc0b82f01a99b27c9a78637c24
Pat <pat@patsullivan.org>
2025-05-13 15:19:32 +0000

    feat(plugin-storage-fs): Initial check-in of the File System Storage plugin

---
+++ packages/plugin-storage-fs/jest.config.ts
@@ -0,0 +1,7 @@
+import { getJestConfig } from "@storm-software/testing-tools";
+
+export default getJestConfig(
+  "packages/plugin-storage-fs",
+  true,
+  "plugin-storage-fs"
+);
---
+++ packages/plugin-storage-fs/src/index.ts
@@ -0,0 +1,43 @@
+/* -------------------------------------------------------------------
+
+                  ⚡ Storm Software - Storm Stack
+
+ This code was released as part of the Storm Stack project. Storm Stack
+ is maintained by Storm Software under the Apache-2.0 license, and is
+ free for commercial and private use. For more information, please visit
+ our licensing page at https://stormsoftware.com/projects/storm-stack/license.
+
+ Website:                  https://stormsoftware.com
+ Repository:               https://github.com/storm-software/storm-stack
+ Documentation:            https://stormsoftware.com/projects/storm-stack/docs
+ Contact:                  https://stormsoftware.com/contact
+
+ SPDX-License-Identifier:  Apache-2.0
+
+ ------------------------------------------------------------------- */
+
+import { getFileHeader } from "@storm-stack/core/helpers/utilities/file-header";
+import type { Options } from "@storm-stack/core/types";
+import type { StoragePluginConfig } from "@storm-stack/devkit/plugins/storage";
+import StoragePlugin from "@storm-stack/devkit/plugins/storage";
+import type { FSStorageOptions } from "unstorage/drivers/fs-lite";
+
+export type StorageFileSystemPluginConfig = FSStorageOptions &
+  StoragePluginConfig;
+
+export default class StorageFileSystemPlugin<
+  TOptions extends Options = Options
+> extends StoragePlugin<TOptions> {
+  public constructor(protected override config: StorageFileSystemPluginConfig) {
+    super(config, "storage-fs-plugin", "@storm-stack/plugin-storage-fs");
+  }
+
+  protected override writeStorage() {
+    return `${getFileHeader()}
+
+import fsLiteDriver from "unstorage/drivers/fs-lite";
+
+export default fsLiteDriver({ base: ${this.config.base ? `"${this.config.base}"` : "undefined"}, readOnly: ${Boolean(this.config.readOnly)}, noClear: ${Boolean(this.config.noClear)} });
+`;
+  }
+}
