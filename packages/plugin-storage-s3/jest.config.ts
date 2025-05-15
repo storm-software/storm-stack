fe6dbc13140944eb9e3d82f851a3f139fde7dfc3
Pat <pat@patsullivan.org>
2025-05-15 02:54:38 +0000

    feat(plugin-storage-s3): Initial check-in of the S3 bucket storage plugin

---
+++ packages/plugin-storage-s3/jest.config.ts
@@ -0,0 +1,3 @@
+import { getJestConfig } from "@storm-software/testing-tools";
+
+export default getJestConfig("packages/plugin-storage-s3", true, "plugin-storage-s3");
---
+++ packages/plugin-storage-s3/src/index.ts
@@ -0,0 +1,59 @@
+/* -------------------------------------------------------------------
+
+                  ⚡ Storm Software - Storm Stack
+
+ This code was released as part of the Storm Stack project. Storm Stack
+ is maintained by Storm Software under the Apache-2.0 license, and is
+ free for commercial and private use. For more information, please visit
+ our licensing page at https://stormsoftware.com/projects/storm-stack/license.
+
+ Website:                  https://stormsoftware.com
+ Repository:               https://github.com/storm-software/storm-stack
+ Documentation:            https://stormsoftware.com/projects/storm-stack/docs
+ Contact:                  https://stormsoftware.com/contact
+
+ SPDX-License-Identifier:  Apache-2.0
+
+ ------------------------------------------------------------------- */
+
+import { getFileHeader } from "@storm-stack/core/helpers/utilities/file-header";
+import type { Options } from "@storm-stack/core/types";
+import type { StoragePluginConfig } from "@storm-stack/devkit/plugins/storage";
+import StoragePlugin from "@storm-stack/devkit/plugins/storage";
+import type { S3DriverOptions } from "unstorage/drivers/s3";
+
+export type StorageS3PluginPluginConfig = StoragePluginConfig & S3DriverOptions;
+
+export default class StorageS3Plugin<
+  TOptions extends Options = Options
+> extends StoragePlugin<TOptions> {
+  protected override installs = {
+    "aws4fetch@1.0.20": "dependency"
+  } as Record<string, "dependency" | "devDependency">;
+
+  public constructor(protected override config: StorageS3PluginPluginConfig) {
+    super(config, "storage-s3-plugin", "@storm-stack/plugin-storage-s3");
+  }
+
+  protected override writeStorage() {
+    return `${getFileHeader()}
+
+import s3Driver from "unstorage/drivers/s3";
+
+const accessKey = process.env.AWS_ACCESS_KEY_ID || $storm.vars.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY || $storm.vars.AWS_SECRET_ACCESS_KEY;
+
+if (!accessKey && !secretAccessKey) {
+  throw new StormError({ type: "general", code: 15 });
+}
+
+export default s3Driver({
+  accessKeyId: accessKey,
+  secretAccessKey: secretAccessKey,
+  endpoint: \`https://s3.${this.config.region}.amazonaws.com/\`,
+  bucket: "${this.config.bucket}",
+  region: "${this.config.region}",
+});
+`;
+  }
+}
