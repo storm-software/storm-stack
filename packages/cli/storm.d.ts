4719dc915ad8efc770b64ae48a75dec87f340205
Pat <pat@patsullivan.org>
2025-06-20 07:58:41 +0000

    chore(monorepo): Update generated example files

--- packages/cli/storm.d.ts
+++ packages/cli/storm.d.ts
@@ -1730,7 +1730,7 @@ declare module "storm:env" {
   /** The current application */
   export const name = "storm-stack";
   /** The current application */
-  export const version = "0.11.0";
+  export const version = "0.11.1";
   /**
    * The environment paths for storing things like data, config, logs, and cache in the current runtime environment.
    *
