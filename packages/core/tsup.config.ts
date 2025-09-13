f450693bfa999e0157908792ed6ebf0e828443eb
Pat <pat@patsullivan.org>
2025-09-13 01:03:43 +0000

    fix(core): Remove previous `scripts` build configuration

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -88,15 +88,6 @@ const config = defineTsupConfig([
     target: "esnext",
     clean: false,
     noExternal: ["@deepkit/type-compiler"]
-  },
-  {
-    name: "core-scripts",
-    entry: ["scripts/post-install.ts"],
-    outDir: "bin",
-    platform: "node",
-    target: "node22",
-    clean: true,
-    noExternal: ["@storm-stack/core/deepkit/type-compiler"]
   }
 ]);
 
